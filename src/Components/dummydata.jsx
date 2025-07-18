757dd3c7330e4a179740fe069b11cfda32c216e5
Axait-th <axaitth@gmail.com>
2025-07-18 21:45:05 +0500

    commit for debugging 1

---
+++ src/Components/dummydata.jsx
@@ -0,0 +1,33 @@
+// Here is dummy data for recipe to be used in the application
+export const dummyData = [
+    {
+        id: 1,
+        name: "Flour",
+        description: "A basic ingredient used in many baking recipes.",
+        amount: 0
+    },
+    {
+        id: 2,
+        name: "Sugar",
+        description: "Sweetens recipes and adds texture to baked goods.",
+        amount: 0
+    },
+    {
+        id: 3,
+        name: "Butter",
+        description: "Adds richness and flavor to dishes and pastries.",
+        amount: 0
+    },
+    {
+        id: 4,
+        name: "Eggs",
+        description: "Provides structure and moisture to recipes.",
+        amount: 0
+    },
+    {
+        id: 5,
+        name: "Milk",
+        description: "Used for moisture and to enhance texture in recipes.",
+        amount: 0
+    }
+];
--- src/Components/ingredientsmenu.jsx
+++ src/Components/ingredientsmenu.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import AmountInput from './input/amouninput.jsx';
+import { useState } from 'react';
+import { dummyData } from './dummydata.jsx';
 
 const IngredientsMenu = () => {
+  // State to manage the active accordion
+  const [activeAccordion, setActiveAccordion] = useState(null);
+
+  // Toggle accordion open/close
+  const handleAccordionClick = (id) => {
+    setActiveAccordion(activeAccordion === id ? null : id);
+  };
+
+
   return (
     <div className="ingredients-menu w-[500px] h-[calc(100vh-300px)] border-[3px] border-red-500 bg-[#FFF6DE] my-[20px] rounded-[20px]">
       <h2 className='ingredients-menu__title'>Ingredients Menu</h2>
@@ -9,38 +20,28 @@ const IngredientsMenu = () => {
         <ul className='bg-blue-100 p-4 rounded-[15px] w-full h-[calc()] overflow-y-auto'>
 
           {/* From here a list starts */}
-          <li>
-            <div className='ingredients-name_and_amount' >
-              <label htmlFor="flour">Flour</label>
-              <div className="ingredients-amount-input">
-                <AmountInput />
-              </div>
-
+          {dummyData.map((ingredient) => (
+            { console.log(ingredient.id) }
+            <li key={ingredient.id} onClick={() => handleAccordionClick(ingredient.id)}>
+          <div className='ingredients-name_and_amount' >
+            <label htmlFor={ingredient.name}>{ingredient.name}</label>
+            <div className="ingredients-amount-input">
+              <AmountInput />
             </div>
-            <p className="description">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium magni, tempora fuga sint odio ut.
-            </p>
-          </li>
-          {/* here list ends */}
 
-          {/* From here a list starts */}
-          <li>
-            <div className='ingredients-name_and_amount' >
-              <label htmlFor="flour">Flour</label>
-              <div className="ingredients-amount-input">
-                <AmountInput />
-              </div>
+          </div>
+          <p className={`description ${activeAccordion === ingredient.id ? 'show' : 'hidden'}`}>
+            {ingredient.description}
+          </p>
+        </>
+        )
+        )
+        }
+        {/* here list ends */}
 
-            </div>
-            <p className="description">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium magni, tempora fuga sint odio ut.
-            </p>
-          </li>
-          {/* here list ends */}
-
-        </ul>
-      </div>
+      </ul>
     </div>
+    </div >
   )
 }
 
